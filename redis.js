e6959bb667408a8214bb03625d512251f65891fe
Swathysuriyababu12 <swathysuryababu@gmail.com>
2023-09-15 16:29:35 +0530

    redis url new

--- redis.js
+++ redis.js
@@ -1,42 +1,35 @@
-const redis = require('redis');
+const redis = require("redis");
 const dotenv = require("dotenv");
 dotenv.config();
 // Internal Redis URL, extract the details into environment variables.
 // "redis://red-xxxxxxxxxxxxxxxxxxxx:6379"
 
-
-
 const redisClient = () => {
-    console.log("redis", process.env.REDIS_SERVICE_NAME, process.env.REDIS_PORT);
-    return redis.createClient({
-
-        host: "rediss://red-ck2397821fec73b0cmv0:Nelc9qMXhmbJDahgUXscVuvi6xrzx0Bt@singapore-redis.render.com",
-        port: 6379
-
-        // url:"rediss://red-ck2397821fec73b0cmv0:Nelc9qMXhmbJDahgUXscVuvi6xrzx0Bt@singapore-redis.render.com:6379"
-
-    });
+  console.log("redis", process.env.REDIS_SERVICE_NAME, process.env.REDIS_PORT);
+  return redis.createClient({
+    // host: "rediss://red-ck2397821fec73b0cmv0:Nelc9qMXhmbJDahgUXscVuvi6xrzx0Bt@singapore-redis.render.com",
+    // port: 6379,
+    url: "rediss://red-ck2397821fec73b0cmv0:Nelc9qMXhmbJDahgUXscVuvi6xrzx0Bt@singapore-redis.render.com:6379",
+  });
 };
 
 const client = redisClient();
 
-client.on('error', (err) => {
-    console.error('Redis Error:', err);
+client.on("error", (err) => {
+  console.error("Redis Error:", err);
 });
 
-client.on('connect', () => {
-    console.log('Connected to Redis');
+client.on("connect", () => {
+  console.log("Connected to Redis");
 });
-client.on('end', () => {
-    console.log('Redis connection ended');
-})
+client.on("end", () => {
+  console.log("Redis connection ended");
+});
 
-client.on('SIGQUIT', () => {
-    client.quit();
-})
-
-
+client.on("SIGQUIT", () => {
+  client.quit();
+});
 
 //Configure session middleware
 
-module.exports = client
+module.exports = client;
